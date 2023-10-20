53048b3c97ac228c5f6dacdfd90dc05786dd44fc
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-10-20 19:03:36 +0330

    add color style to components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,57 @@
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState<Boolean>(false);
+
   return (
-    <main className="grid gap-4 md:grid-cols-12 md:grid-rows-2">
-      <div className="hidden md:block md:row-span-2 md:col-span-3 bg-blue-200">
-        <div>
+    <main className="grid gap-4 md:grid-cols-12 md:grid-rows-2 bg-gray-50 px-4 py-2 min-h-screen">
+      <div className="hidden md:block md:row-span-2 md:col-span-3">
+        <div className="bg-white rounded-2xl overflow-hidden">
           {/* accordion header */}
-          <div className="flex justify-between">
+          <div
+            className="flex justify-between p-4 cursor-pointer"
+            onClick={() => setIsOpen(!isOpen)}
+          >
             <span>دسته بندی مقالات</span>
-            <ChevronDownIcon className="stroke-blue-400 w-6 h-6" />
+            <ChevronDownIcon
+              className={`stroke-yellow-400 w-6 h-6 transition-all ${
+                isOpen ? "rotate-180" : "rotate-0"
+              }`}
+            />
           </div>
 
           {/* accordion content */}
-          <ul>
-            <li>نکست جی‌اس</li>
-            <li>نکست جی‌اس</li>
-            <li>نکست جی‌اس</li>
-            <li>نکست جی‌اس</li>
-            <li>نکست جی‌اس</li>
+          <ul
+            className={`transition-all ${
+              isOpen ? "block" : "hidden"
+            } [&_a]:pr-4 [&_a]:block [&>*]:py-2 [&>*]:transition-all hover:[&>*]:bg-amber-100`}
+          >
+            <li>
+              <Link href="#">نکست جی‌اس</Link>
+            </li>
+            <li>
+              <Link href="#">نکست جی‌اس</Link>
+            </li>
+            <li>
+              <Link href="#">نکست جی‌اس</Link>
+            </li>
+            <li>
+              <Link href="#">نکست جی‌اس</Link>
+            </li>
+            <li>
+              <Link href="#">نکست جی‌اس</Link>
+            </li>
           </ul>
         </div>
       </div>
-      <div className="bg-blue-200 hidden md:block md:col-span-9">
+      <div className="bg-yellow-400 px-4 py-2 rounded-2xl hidden md:block md:col-span-9">
         مرتب سازی بر اساس
       </div>
-      <div className="bg-blue-200 md:block md:col-span-9">مقالات</div>
+      <div className="bg-yellow-400 px-4 py-2 rounded-2xl md:block md:col-span-9 md:row-span-2">
+        مقالات
+      </div>
     </main>
   );
 }
