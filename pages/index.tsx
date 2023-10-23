ec69d5358980e81f4b6ee237fb93026d5b4b6b44
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-10-23 17:21:59 +0330

    add category bar for mobile screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,6 +68,25 @@ export default function Home({ data, categories }: HomeProps) {
           </ul>
         </div>
       </div>
+      {/* category bar mobile */}
+      <div className="block md:hidden overflow-x-auto">
+        <ul className="flex flex-nowrap gap-2">
+          <li className="px-3 py-1 border border-slate-200 rounded-2xl whitespace-nowrap">
+            <Link href="#">همه مقالات</Link>
+          </li>
+          {categories.data.map((category: Category) => (
+            <li
+              key={category?._id}
+              className="px-3 py-1 border border-slate-200 rounded-2xl "
+            >
+              <Link href={`/blogs/${category?.englishTitle}`}>
+                {category?.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {/* sort bar desktop */}
       <div className="bg-yellow-100 px-4 py-2 rounded-2xl hidden md:flex items-center gap-x-2 md:col-span-9">
         <AdjustmentsHorizontalIcon className="stroke-yellow-950 w-6" />
         <p className="ml-8">مرتب سازی بر اساس</p>
